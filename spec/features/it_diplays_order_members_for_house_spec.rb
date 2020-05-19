a12ef083283a2ed1a89e44ca0b315cb90df75b55
Fred Rondina <fredrondina96@gmail.com>
2020-05-19 10:45:53 -0600

    Added additional tests, now all houses are accounted for

--- spec/features/it_diplays_order_members_for_house_spec.rb
+++ spec/features/it_diplays_order_members_for_house_spec.rb
@@ -4,8 +4,24 @@ describe "From the welcome page I can select a house" do
   it 'and all of its phoneix members will display' do
    visit root_path
    click_button "Search For Members"
-
    expect(page).to have_content("Gryffindor")
    expect(page).to have_content("21 Total Members")
+
+   visit root_path
+   page.select 'Hufflepuff', from: :house
+   click_button "Search For Members"
+   expect(page).to have_content("3 Total Members")
+   expect(page).to have_content("Hufflepuff")
+
+   visit root_path
+   page.select 'Slytherin', from: :house
+   click_button "Search For Members"
+   expect(page).to have_content("1 Total Members")
+   expect(page).to have_content("Slytherin")
+
+   visit root_path
+   page.select 'Ravenclaw', from: :house
+   click_button "Search For Members"
+   expect(page).to have_content("No Order of the Phoenix Members for selected house")
   end
 end
